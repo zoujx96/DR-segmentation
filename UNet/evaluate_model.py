03e20c8c96e5c8c8565275191ee4260afcb09f9c
Jiaxu Zou <zoujx96@gmail.com>
2019-05-26 00:34:39 -0400

    Add files via upload

---
+++ UNet/evaluate_model.py
@@ -0,0 +1,156 @@
+"""
+File: train.py
+Created by: Qiqi Xiao
+Email: xiaoqiqi177<at>gmail<dot>com
+"""
+
+import sys
+from torch.autograd import Variable
+import os
+from optparse import OptionParser
+import numpy as np
+import random
+import copy
+from sklearn.metrics import precision_recall_curve, average_precision_score, f1_score
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch.optim import lr_scheduler
+
+import config
+from unet import UNet
+from hednet import HNNNet
+from utils import get_images
+from dataset import IDRIDDataset
+from torchvision import datasets, models, transforms
+from transform.transforms_group import *
+from torch.utils.data import DataLoader, Dataset
+import argparse
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+net_name = config.NET_NAME
+image_size = config.IMAGE_SIZE
+image_dir = config.IMAGE_DIR
+
+def eval_model(model, eval_loader):
+    model.eval()
+    masks_soft = []
+    masks_hard = []
+
+    with torch.set_grad_enabled(False):
+        for inputs, true_masks in eval_loader:
+            inputs = inputs.to(device=device, dtype=torch.float)
+            true_masks = true_masks.to(device=device, dtype=torch.float)
+            bs, _, h, w = inputs.shape
+            # not ignore the last few patches
+            h_size = (h - 1) // image_size + 1
+            w_size = (w - 1) // image_size + 1
+            masks_pred = torch.zeros(true_masks.shape).to(dtype=torch.float)
+
+            for i in range(h_size):
+                for j in range(w_size):
+                    h_max = min(h, (i + 1) * image_size)
+                    w_max = min(w, (j + 1) * image_size)
+                    inputs_part = inputs[:,:, i*image_size:h_max, j*image_size:w_max]
+                    if net_name == 'unet':
+                        masks_pred_single = model(inputs_part)
+                    elif net_name == 'hednet':
+                        masks_pred_single = model(inputs_part)[-1]
+                    
+                    masks_pred[:, :, i*image_size:h_max, j*image_size:w_max] = masks_pred_single
+
+            masks_pred_softmax_batch = F.softmax(masks_pred, dim=1).cpu().numpy()
+            masks_soft_batch = masks_pred_softmax_batch[:, 1:, :, :]
+            masks_hard_batch = true_masks[:,1:].cpu().numpy()
+
+            masks_soft.extend(masks_soft_batch)
+            masks_hard.extend(masks_hard_batch)
+
+    masks_soft = np.array(masks_soft).transpose((1, 0, 2, 3))
+    masks_hard = np.array(masks_hard).transpose((1, 0, 2, 3))
+    masks_soft = np.reshape(masks_soft, (masks_soft.shape[0], -1))
+    masks_hard = np.reshape(masks_hard, (masks_hard.shape[0], -1))
+
+    #masks_soft = masks_soft.round(2)
+    masks_hard = masks_hard[0].astype(np.int)
+    #masks_hard = masks_hard[0]
+    masks_soft = masks_soft[0].round().astype(np.int)
+
+    f1 = f1_score(masks_hard, masks_soft)
+    
+    #ap = average_precision_score(masks_hard, masks_soft)
+    #precisions, recalls, _ = precision_recall_curve(masks_hard, masks_soft)
+    '''
+    thresholds = np.linspace(0, 1, 33)
+    thresholds = thresholds[:-1]
+    new_precisions = []
+    new_recalls = []
+    for threshold in thresholds:
+        new_precisions.append(precision_score(masks_hard, (masks_soft >= threshold).astype(np.int)))
+        new_recalls.append(recall_score(masks_hard, (masks_soft >= threshold).astype(np.int)))
+
+    new_precisions.append(1.)
+    new_recalls.append(0.)
+
+    new_precisions = np.array(new_precisions)
+    new_recalls = np.array(new_recalls)
+
+    new_precisions = new_precisions[np.argsort(new_recalls)]
+    new_recalls = np.sort(new_recalls)
+    auc_result = auc(new_recalls, new_precisions)
+    '''
+    #return ap, precision, recall
+    #return auc_result
+    #return ap
+    return f1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--preprocess', type=str, default='7')
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--lesion', type=str, default='EX')
+    args = parser.parse_args()
+    #Set random seed for Pytorch and Numpy for reproducibility
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    if net_name == 'unet':
+        model = UNet(n_channels=3, n_classes=2)
+    else:
+        model = HNNNet(pretrained=True, class_number=2)
+
+    resume = args.model
+
+    if os.path.isfile(resume):
+        print("=> loading checkpoint '{}'".format(resume))
+        checkpoint = torch.load(resume)
+        model.load_state_dict(checkpoint['state_dict'])
+        print('Model loaded from {}'.format(resume))
+    else:
+        print("=> no checkpoint found at '{}'".format(resume))
+
+    model.to(device)
+
+    test_image_paths, test_mask_paths = get_images(image_dir, args.preprocess, phase='test')
+
+    if net_name == 'unet':
+        test_dataset = IDRIDDataset(test_image_paths, test_mask_paths, config.LESION_IDS[args.lesion])
+    elif net_name == 'hednet':
+        test_dataset = IDRIDDataset(test_image_paths, test_mask_paths, config.LESION_IDS[args.lesion], \
+            transform=Compose([Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),]))
+
+    test_loader = DataLoader(test_dataset, 1, shuffle=False)
+    auc_result = eval_model(model, test_loader)
+    print(auc_result)
+
+    
