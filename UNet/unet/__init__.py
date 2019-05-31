415c4290470c2c5586ddd96e0105db07511c9250
Jiaxu Zou <zoujx96@gmail.com>
2019-05-31 00:59:46 -0400

    Update __init__.py

--- UNet/unet/__init__.py
+++ UNet/unet/__init__.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# **************************************
+# @Author  : Qiqi Xiao
+# @Email     : xiaoqiqi177@gmail.com
+# @File    : __init__.py
+# **************************************
 # Get code from https://github.com/milesial/Pytorch-UNet
 
 from .unet_model import UNet
